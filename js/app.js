273d47c16d5a89d959c95d766900b964716087c3
jplourd <jeffplourd@gmail.com>
2015-01-23 12:17:58 -0800

    Added a 'speed' variable for each instance of 'Enemy' and added functionality to the 'Enemy.prototype.update' function.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed) {
     // I decided to create an 'x' and 'y' variable for each block
     // and set them equal to the starting block they should occupy
     this.x = x*101;
     this.y = y*83;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -15,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt * speed;
 }
 
 // Draw the enemy on the screen, required method for game
